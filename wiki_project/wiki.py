f53888b8a7ad5c268164a1a2adcfbe4a9c9bc83e
LaraMi92 <laraminski@hotmail.fr>
2021-04-14 09:47:19 +0200

    initial commit

---
+++ wiki_project/wiki.py
@@ -0,0 +1,42 @@
+
+import wikipedia
+import sqlite3
+
+#wikipedia searches
+userinput = raw_input("Which famous person are you looking for?\n")
+def search(value):
+  return wikipedia.search(value, results=1)
+
+research = wikipedia.suggest(userinput)
+if research == None:
+    research = search(userinput)
+    
+else:
+    print("Did you mean " + research + " ?\n")
+    research = search(research)
+    
+print("Here is what we found: " + research[0])
+summary = wikipedia.summary(research) 
+
+#database queries
+def sendtodb(research, summary):
+    try:
+        con = sqlite3.connect('wiki.db')
+        cur = con.cursor()
+        cur.execute("""CREATE TABLE IF NOT EXISTS famous_people
+                    (name TEXT, summary TEXT)""")
+        cur.execute("""INSERT INTO famous_people(name, summary)
+                    VALUES(?, ?)""", (research, summary))
+        con.commit()
+        cur.close()
+        print("Amazing, " + research + " has been added!")
+
+    except sqlite3.Error as error:
+        print("Oops it seems there was an error while sending data...", error)
+
+    finally:
+        if con:
+            con.close()
+            print("End of process")
+
+sendtodb(research[0], summary)
